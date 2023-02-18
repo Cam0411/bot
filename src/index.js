ada5cbf3a584f9820c8a101f6b2a8a5395498110
Cam0411 <89090731+Cam0411@users.noreply.github.com>
2023-02-18 23:42:22 +0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 
+const { Client, GatewayIntentBits } = require('discord.js')
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        // GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ]
+})
+client.on('ready', () =>{
+console.log('works')
+})
+
+client.on('interactionCreate', (interaction) => {
+    if (interaction.isCommand() && interaction.commandName === 'ping') {
+        interaction.reply({content: 'Pong!'});
+    }
+});
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) {
+      return;
+    }
+    if (message.content === "hello"){
+        message.reply("Welcome Back Cam")
+    }
+    if (message.content === `random`) {
+        var rating = Math.floor(Math.random() * 100) + 1;
+        message.reply(`the random value is ${rating}`)
+     }
+  });
+
+
+
+
+client.login('MTA3NjUyNDk2OTYzNjQxMzQ0MQ.GUUYKb.rNPjdtF-fEuaN_5lmCuNakqcIey8yfYQTt9irg')
+
+
